f9a23cf63b763ce2b85ed1cc87371ec3f71c386f
Ivan.Perkhun <Ivan.Perkhun@budu.ru>
2024-09-09 10:19:14 +0300

    add cancel_scheduled_messages

--- modules/auto_messaging.py
+++ modules/auto_messaging.py
@@ -1,26 +1,29 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime, timedelta
 from aiogram import Bot
-import asyncio
+from utils.content import MESSAGES
 
-MESSAGES = {
-    1: "Поздравляю! Уже через 1 час после отказа от сигарет твое сердце начинает биться ровнее, и давление приходит в норму. Ты уже на правильном пути!",
-    3: "Прошло 3 часа без курения! Твой организм начинает снижать уровень никотина в крови. Продолжай, ты отлично справляешься!",
-    5: "С момента последней сигареты прошло 5 часов. Кровообращение улучшается, и твое тело начинает восстанавливаться. Ты делаешь важные шаги к здоровью!",
-    12: "Прошло 12 часов! Уровень угарного газа в крови снизился до нормы, и твое тело получает больше кислорода. Дышать становится легче!"
-}
+scheduler = AsyncIOScheduler()
 
-# Функция для отправки сообщений
 async def send_message(bot: Bot, user_id: int, text: str):
     await bot.send_message(user_id, text, parse_mode="Markdown")
 
-# Планировщик сообщений
+
 def schedule_messages(bot: Bot, user_id: int, start_time: datetime):
-    scheduler = AsyncIOScheduler()
-    # Планируем каждое сообщение
+    job_ids = []
+
     for hours, message in MESSAGES.items():
-        send_time = start_time + timedelta(seconds=hours)
-        scheduler.add_job(send_message, 'date', run_date=send_time, args=[bot, user_id, message])
+        send_time = start_time + timedelta(hours=hours)
+        job = scheduler.add_job(send_message, 'date', run_date=send_time, args=[bot, user_id, message])
+        job_ids.append(job.id)  # Сохраняем идентификаторы задач
     
     scheduler.start()
+    return job_ids
 
+
+def cancel_scheduled_messages(job_ids):
+    for job_id in job_ids:
+        try:
+            scheduler.remove_job(job_id)
+        except Exception as e:
+            print(f"Ошибка при отмене задачи {job_id}: {e}")
--- modules/stop_smoking.py
+++ modules/stop_smoking.py
@@ -1,19 +1,24 @@
-from aiogram import Bot
+from modules.auto_messaging import schedule_messages, cancel_scheduled_messages
 from db.data_manager import get_user_data, update_user_data
-from aiogram import types
-from modules.auto_messaging import schedule_messages
 from datetime import datetime
+from aiogram import types, Bot
 
-# Функция для обработки команды /stop_smoking
 async def cmd_stop_smoking(message: types.Message, bot: Bot):
     user_id = message.from_user.id
     user_data = get_user_data(user_id)
     
+    # Если у пользователя есть активные задачи, отменяем их
+    if 'job_ids' in user_data:
+        cancel_scheduled_messages(user_data['job_ids'])
     
+    # Устанавливаем новое время отказа от курения
     stop_time = datetime.now()
     user_data['stop_smoking_time'] = stop_time.strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Планируем новые напоминания и сохраняем job_ids
+    job_ids = schedule_messages(bot, user_id, stop_time)
+    user_data['job_ids'] = job_ids
     update_user_data(user_id, user_data)
-    schedule_messages(bot, user_id, stop_time)
 
     await message.answer(
         "Ты решил бросить курить! Я буду поддерживать тебя на этом пути и присылать сообщения с прогрессом."
